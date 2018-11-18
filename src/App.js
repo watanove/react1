c4751dfc3d4f9dd82fdad4bf39b6cfdc80a55ef2
nove <watanove@noveltte.jp>
2018-11-18 18:37:18 +0900

    Component class,function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-
-    const con="conJSX"
-    const x=<React.Fragment>
-      <label htmlFor="xxx">へへー</label>
-      <input id="xxx" type="text" className="houhou"  onChange={()=>{console.log(1)}}/>
-    </React.Fragment>
-    return x//<h1>Hello!!!</h1>
-  }
-}
+// class App extends Component {
+//   render() {
+//
+//     const con="conJSX"
+//     const x=<React.Fragment>
+//       <label htmlFor="xxx">へへー</label>
+//       <input id="xxx" type="text" className="houhou"  onChange={()=>{console.log(1)}}/>
+//     </React.Fragment>
+//     return x//<h1>Hello!!!</h1>
+//   }
+// }
 // class App extends Component {
 //   render() {
 //     return React.createElement(トランスパイルの時にはこれが行われるので
@@ -22,4 +22,12 @@ class App extends Component {
 //   }
 // }
 
+const App=()=>{
+  return<div>
+    <Ban/>
+    <Ban/>
+  </div>
+}
+
+const Ban=()=><div>IamChampion</div>
 export default App;
