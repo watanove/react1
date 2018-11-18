965bb6735b50adcdb5d3a62ac8c91b34443f4c75
nove <watanove@noveltte.jp>
2018-11-19 07:10:15 +0900

    ok

--- src/App.js
+++ src/App.js
@@ -1,43 +1,8 @@
-import React from 'react';
-import Proptypes from 'prop-types';
-// class App extends Component {
-//   render() {
-//
-//     const con="conJSX"
-//     const x=<React.Fragment>
-//       <label htmlFor="xxx">へへー</label>
-//       <input id="xxx" type="text" className="houhou"  onChange={()=>{console.log(1)}}/>
-//     </React.Fragment>
-//     return x//<h1>Hello!!!</h1>
-//   }
-// }
-// class App extends Component {
-//   render() {
-//     return React.createElement(トランスパイルの時にはこれが行われるので
-// import Reactは必須なのです。
-//       "div",
-//       null,
-//       "<span style='color:red;'>Hello!!!!!!!</span>"
-//     )
-//   }
-// }
+import React,{Component} from 'react';
 
-const App=()=>{
-  const x=[{name:"ban",age:10},{name:"che",agex:20}]
-  return<div>
-    {x.map(function(xx){return <Ban key={xx.name+xx.age} name={xx.name} age={xx.age} />})}
-  </div>
-}
+const App=()=>(<Counter/>)
 
-
-const Ban=(props)=><div>{props.name}IamChampion and aged{props.age}</div>
-
-// Ban.defaultProps={
-//   age:100
-// }
-
-Ban.propTypes={
-  name:Proptypes.string,
-  age:Proptypes.number.isRequired
+class Counter extends Component{
+  render=()=>(<div>へろー</div>)
 }
 export default App;
