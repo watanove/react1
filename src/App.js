e838eec2f0c284186efb90b9800eb33881c5f7ce
nove <watanove@noveltte.jp>
2018-11-20 04:29:58 +0900

    ii

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React,{Component} from 'react';
 
 const App=()=>(<Counter/>)
+let o={
+x:function(){
+  console.log('hello')
+}
+}
+
+var o2={
+  x(){},
+  y:function(){}
+}
+
 
 class Counter extends Component{
-  render=()=>(<div>へろー</div>)
+  constructor (props){
+    super(props)
+    console.log(this.state)
+    this.state={count:0}
+  }
+  handleButton=()=>{
+    console.log(this.state)
+  }
+  render(){
+    console.log(this.state)
+    return (<React.Fragment><div>this.state.count={this.state.count}</div>
+      <button onClick={this.handleButton}>おしてー</button></React.Fragment>
+    )
+  }
 }
 export default App;
